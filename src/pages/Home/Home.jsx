882ad01cc4ab35cf98b40bc028867cd29bd08873
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-10-21 16:42:17 -0500

    deploy finish

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ export default function Home () {
 
     const descargar = () => {
         const enlace = document.createElement('a');
-        enlace.href = '/Hoja de vida Marcos (4).pdf'; 
+        enlace.href = '/Hoja de vida Marcos (5).pdf'; 
         enlace.download = 'HojadevidaMarcos.pdf';
         enlace.click();
       };
